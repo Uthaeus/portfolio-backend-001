e435894385e45d5b6d9c4578115b4e8744fbb38b
Uthaeus <romanlavery@gmail.com>
2023-06-30 10:14:28 -0600

    resolved bug with creating new skill

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,5 @@
 class Skill < ApplicationRecord
-    validates_presence_of :name, :percent_utilized
+    validates_presence_of :title, :percent_utilized
     
     mount_uploader :icon_image, SkillUploader
 end
